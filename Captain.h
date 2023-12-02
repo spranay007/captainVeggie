eefefaad4c2e47380217cbbad83dac365718c03b
Affy23 <agowani@stevens.edu>
2023-12-02 18:05:32 -0500

    Work in progress Two more files added - Captain.cpp & Captain.h

---
+++ Captain.h
@@ -0,0 +1,25 @@
+#ifndef CAPTAIN_H
+#define CAPTAIN_H
+
+#include "Creature.h"
+#include "Veggie.h"  
+
+class Captain : public Creature 
+{
+private:
+    vector<Veggie*> veggieCollection;
+
+public:
+    Captain(int xCoord, int yCoord);
+
+    // Function to add Veggie to the collection
+    void addVeggie(Veggie* veggie);
+
+    // Getter function for veggieCollection
+    vector<Veggie*>& getVeggieCollection();
+
+    // Setter function for veggieCollection
+    void setVeggieCollection(const vector<Veggie*>& collection);
+};
+
+#endif // CAPTAIN_H
