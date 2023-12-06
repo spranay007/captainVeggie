f9bcfc2b2f7c94e5fc4234e4961f17f687b32f8d
SamirBhalala <sambhalala20@gmail.com>
2023-12-06 16:34:47 -0500

    Comment for caption file

--- Captain.cpp
+++ Captain.cpp
@@ -4,27 +4,31 @@ Description:
 
 */
 
-
+// Include the header file for the Captain class
 #include "Captain.h"
 
+// Constructor for the Captain class, initializing the Creature base class with coordinates and a string identifier
 Captain::Captain(int xCoord, int yCoord) : Creature(xCoord, yCoord, "V") {}
 
+// Method to add a Veggie object to the veggieCollection vector
 void Captain::addVeggie(Veggie* veggie) 
 {
     veggieCollection.push_back(veggie);
 }
 
+// Getter method to retrieve the veggieCollection vector
 const vector<Veggie*>& Captain::getVeggieCollection() const
 {
     return veggieCollection;
 }
 
+// Setter method to set the veggieCollection vector with a new collection
 void Captain::setVeggieCollection(const vector<Veggie*>& collection)
- {
-    // Clear existing collection and copy the new one
+{
+    // Clear existing collection to avoid memory leaks and copy the new one
     for (Veggie* veggie : veggieCollection) 
     {
-        delete veggie;
+        delete veggie; // Delete each Veggie object in the existing collection
     }
-    veggieCollection = collection;
+    veggieCollection = collection; // Assign the new collection to veggieCollection
 }
